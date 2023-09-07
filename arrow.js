418b6d470d417347e75fe6e583b840776c71b896
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-08 00:19:15 +0500

    in arrow function , Math.power command used to calculte the area ofcircle

--- arrow.js
+++ arrow.js
@@ -1,6 +1,4 @@
 "use strict";
-// var result=(num1:Number, num2:Number):number=>num1*num2
-// result(5,2)
 var result = (num1, num2) => {
     // let num1:number=10
     // let num2:number=30
@@ -13,4 +11,19 @@ var result = (num1, num2) => {
 //result(10,30)
 var newResult = result(10, 30);
 console.log(newResult);
-// result(5,2)
+var multipl = (num3, num4) => {
+    var multiply = num3 * num4;
+    return multiply;
+};
+var finalmultiply = multipl(3, 5);
+console.log(finalmultiply);
+//Q4 - Write a program that defines a function to calculate the area of a circle.
+// The function should take the radius as input and return the calculated area.
+// area of circle calculated by the formula A=pi*radius square
+var pie = 3.14;
+var areaofCircle = (num1) => {
+    var circleArea = pie * Math.pow(num1, 2);
+    return circleArea;
+};
+var calculatedArea = areaofCircle(5);
+console.log(calculatedArea);
--- arrow.ts
+++ arrow.ts
@@ -1,5 +1,3 @@
-// var result=(num1:Number, num2:Number):number=>num1*num2
-// result(5,2)
 
 var result=(num1:number, num2:number):number=>
 {
@@ -17,4 +15,23 @@ var newResult:number=result(10,30)
 console.log(newResult)
 
 
-// result(5,2)
+var multipl=(num3:number, num4:number):number=>
+{
+    var multiply:number=num3*num4
+    return multiply
+}
+var finalmultiply:number=multipl(3,5)
+console.log(finalmultiply)
+
+
+//Q4 - Write a program that defines a function to calculate the area of a circle.
+// The function should take the radius as input and return the calculated area.
+// area of circle calculated by the formula A=pi*radius square
+
+var pie:number=3.14
+var areaofCircle=(num1:number):number=>{
+    var circleArea:number=pie*Math.pow(num1,2)
+    return circleArea
+}
+var calculatedArea:number=areaofCircle(5)
+console.log(calculatedArea)
