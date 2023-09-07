58a38ccb822be6aa3965f948c85d29dec620c36a
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-08 00:04:28 +0500

    Arrow function method explained

---
+++ arrow.js
@@ -0,0 +1,16 @@
+"use strict";
+// var result=(num1:Number, num2:Number):number=>num1*num2
+// result(5,2)
+var result = (num1, num2) => {
+    // let num1:number=10
+    // let num2:number=30
+    let sum = num1 + num2;
+    // let multipl:number=num1*num2
+    // console.log(sum)
+    // console.log(multipl)
+    return sum;
+};
+//result(10,30)
+var newResult = result(10, 30);
+console.log(newResult);
+// result(5,2)
---
+++ arrow.ts
@@ -0,0 +1,20 @@
+// var result=(num1:Number, num2:Number):number=>num1*num2
+// result(5,2)
+
+var result=(num1:number, num2:number):number=>
+{
+// let num1:number=10
+// let num2:number=30
+let sum:number=num1+num2
+// let multipl:number=num1*num2
+// console.log(sum)
+// console.log(multipl)
+return sum
+}
+//result(10,30)
+
+var newResult:number=result(10,30)
+console.log(newResult)
+
+
+// result(5,2)
