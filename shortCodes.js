e58bf929cc67f63e0becdc8a54ea86a9d4c2224a
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-07 10:03:39 +0500

    short codes file added

---
+++ shortCodes.js
@@ -0,0 +1,12 @@
+"use strict";
+//this is how to take power value of a defined variable
+let num1 = 5;
+console.log(Math.pow(num1, 3));
+console.log(5 ** 2);
+// var squareTake=(num2:number)=>{num2**2}
+// squareTake(5)
+// console.log(square(5))
+let name1 = "asif";
+console.log(name1.toUpperCase());
+let name2 = "ASIF";
+console.log(name1.toLowerCase());
